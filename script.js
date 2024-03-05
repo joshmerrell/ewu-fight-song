dc1647eccac44c7a3ddd5a07c552b893b92a8ad1
Joshua Merrell <84419054+joshmerrell@users.noreply.github.com>
2024-03-05 20:17:48 +0000

    Add button to play/stop song

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-// Check JS is Connected
-console.log('JS Connected');
+console.log("Joshua Merrell");
+
+const playBtn = document.getElementById("play-btn");
+const audioObj = document.querySelector("audio");
+let playing = false;
+playBtn.onclick = function(ev) {
+    if(playing) {
+        console.log("Stopping audio");
+        audioObj.pause();
+    } else {
+        console.log("Playing audio");
+        audioObj.play();
+    }
+    audioObj.fastSeek(0);
+    playing = !playing;
+};
