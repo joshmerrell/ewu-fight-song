b52d013d7bf9e826fb24bd828273b7babf54e33b
Joshua Merrell <84419054+joshmerrell@users.noreply.github.com>
2024-03-07 18:34:55 +0000

    Use paused property

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ console.log("Joshua Merrell");
 
 const playBtn = document.getElementById("play-btn");
 const audioObj = document.querySelector("audio");
-let playing = false;
+
 playBtn.onclick = function(ev) {
-    if(playing) {
-        console.log("Stopping audio");
-        audioObj.pause();
-    } else {
+    audioObj.fastSeek(0);
+    if(audioObj.paused) {
         console.log("Playing audio");
         audioObj.play();
+    } else {
+        console.log("Stopping audio");
+        audioObj.pause();        
     }
-    audioObj.fastSeek(0);
-    playing = !playing;
 };
