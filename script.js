48000b2adda1e6f4723de05b182b46d37c1b8152
Joshua Merrell <84419054+joshmerrell@users.noreply.github.com>
2024-03-07 20:05:53 +0000

    Use Plyr, more style changes

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 console.log("Joshua Merrell");
 
-const playBtn = document.getElementById("play-btn");
-const audioObj = document.querySelector("audio");
-
-playBtn.onclick = function(ev) {
-    audioObj.fastSeek(0);
-    if(audioObj.paused) {
-        console.log("Playing audio");
-        audioObj.play();
-    } else {
-        console.log("Stopping audio");
-        audioObj.pause();        
-    }
-};
+const player = new Plyr("audio", {});
